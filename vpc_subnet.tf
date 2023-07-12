05f879426c70c72f4c097fee2cabfcd151a8c394
iure de oliveira radin <iureradin@hotmail.com>
2023-07-12 01:42:14 -0300

    commit

--- vpc_subnet.tf
+++ vpc_subnet.tf
@@ -1,16 +1,41 @@
 resource "aws_vpc" "example_vpc" {
   cidr_block = var.vpc_cidr_block
+
   tags = {
     "provider" = "terraform"
-    "Name" = "default_vpc"
+    "Name"     = "default_vpc"
+  }
+}
+
+resource "aws_internet_gateway" "example_igw" {
+  vpc_id = aws_vpc.example_vpc.id
+
+  tags = {
+    "provider" = "terraform"
+    "Name"     = "example_igw"
   }
 }
 
 resource "aws_subnet" "example_subnet" {
   vpc_id     = aws_vpc.example_vpc.id
   cidr_block = var.subnet_cidr_block
+
   tags = {
     "provider" = "terraform"
-    "Name" = "sub-priv1"
+    "Name"     = "sub-priv1"
   }
+}
+
+resource "aws_route_table" "example_route_table" {
+  vpc_id = aws_vpc.example_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.example_igw.id
+  }
+}
+
+resource "aws_route_table_association" "example_route_table_association" {
+  subnet_id      = aws_subnet.example_subnet.id
+  route_table_id = aws_route_table.example_route_table.id
 }
